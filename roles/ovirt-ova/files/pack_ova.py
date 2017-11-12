a198b02395f4e3c343ccb1c48a334ae718496e11
Arik Hadas <ahadas@dubi.tlv.redhat.com>
2017-11-12 22:49:04 +0200

    polish the python script

--- roles/ovirt-ova/files/pack_ova.py
+++ roles/ovirt-ova/files/pack_ova.py
@@ -10,44 +10,50 @@ BLOCKSIZE = 512
 NUL = "\0"
 buf = bytearray(4096)
 
-fd = os.open(sys.argv[1], os.O_WRONLY | os.O_CREAT)
-tar = io.FileIO(fd, "w")
-
-print 'writing ovf'
-ovf = sys.argv[2]
-info = tarfile.TarInfo(name="ovf")
-info.size = len(ovf.encode('utf-8'))
-info.mtime = time.time()
-tar.write(info.tobuf())
-tar.write(ovf)
-remainder = info.size % BLOCKSIZE
-if remainder > 0:
-    tar.write(NUL * (BLOCKSIZE - remainder))
-
-for arg in sys.argv[3:]:
-    idx = arg.index('::')
-    path = arg[:idx]
-    size = int(arg[idx+2:])
-    print 'writing disk %s (%d)' % (path, size)
-    fd = os.open(path, os.O_RDONLY)
-    basename = os.path.basename(path)
-    info = tarfile.TarInfo(name=basename)
+def createTarInfo(name, size):
+    info = tarfile.TarInfo(name)
     info.size = size
     info.mtime = time.time()
-    tar.write(info.tobuf())
-    file = io.FileIO(fd, "r+")
-    while 1:
-        if file.readinto(buf) == 0:
-            break
-        tar.write(buf)
-    remainder = info.size % BLOCKSIZE
+    return info
+
+def padToBlockSize(file, size):
+    remainder = size % BLOCKSIZE
     if remainder > 0:
-        tar.write(NUL * (BLOCKSIZE - remainder))
+        file.write(NUL * (BLOCKSIZE - remainder))
+
+ova_path = sys.argv[1]
+print "opening for write: %s" % ova_path
+ova_fd = os.open(ova_path, os.O_WRONLY | os.O_CREAT)
+ova_file = io.FileIO(ova_fd, "w")
+
+ovf = sys.argv[2]
+print "writing ovf: %s" % ovf
+ovf_size = len(ovf.encode('utf-8'))
+tar_info = createTarInfo("ovf", ovf_size)
+ova_file.write(tar_info.tobuf())
+ova_file.write(ovf)
+padToBlockSize(ova_file, ovf_size)
+
+for disk_info in sys.argv[3:]:
+    idx = disk_info.index('::')
+    disk_path = disk_info[:idx]
+    disk_size = int(disk_info[idx+2:])
+    print 'writing disk: path=%s size=%d' % (disk_path, disk_size)
+    disk_name = os.path.basename(disk_path)
+    tar_info = createTarInfo(disk_name, disk_size)
+    ova_file.write(tar_info.tobuf())
+    disk_fd = os.open(disk_path, os.O_RDONLY)
+    disk_file = io.FileIO(disk_fd, "r+")
+    while 1:
+        if disk_file.readinto(buf) == 0:
+            break
+        ova_file.write(buf)
+    padToBlockSize(ova_file, disk_size)
 
 # writing two null blocks at the end of the file
-empty = NUL * 512
-tar.write(empty)
-tar.write(empty)
+empty_block = NUL * 512
+ova_file.write(empty_block)
+ova_file.write(empty_block)
 
-tar.close()
+ova_file.close()
 
